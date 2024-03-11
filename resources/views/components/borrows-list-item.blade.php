679319f83475e7658bd62d9db65313d1223cf62d
uebahide <uebahide@gmail.com>
2024-03-11 17:28:06 +0100

    My rental list unable to be fold

--- resources/views/components/borrows-list-item.blade.php
+++ resources/views/components/borrows-list-item.blade.php
@@ -1,15 +1,15 @@
-@php
-    $bgColor = null;
+  @php
+      $bgColor = null;
 
-  switch ($type) {
-    case 'Pending':
-      $bgColor = "bg-white";
-      break;
-    case 'Accepted':
-      $bgColor = "bg-green-100";
-      break;
-    case 'Late':
-      $bgColor = "bg-red-100";
+    switch ($type) {
+      case 'Pending':
+        $bgColor = "bg-white";
+        break;
+      case 'Accepted':
+        $bgColor = "bg-green-100";
+        break;
+      case 'Late':
+        $bgColor = "bg-red-100";
       break;
     case 'Rejected':
       $bgColor = "bg-gray-200";
@@ -22,22 +22,37 @@
 
 
 
-
-<div class="border rounded shadow-lg p-4 mb-5 {{$bgColor}}">
-    <div class="text-2xl mb-3 text-center">{{$type}}</div>
-    @foreach ($borrows as $borrow)
-    <a href="{{route('user.borrows.show', ['borrow'=> $borrow->id])}}" class="flex flex-col border shadow-md mb-2 bg-white p-2">
-      <p>Title : {{$borrow->book->title}}</p>
-      <p>Authros : {{$borrow->book->authors}}</p>
-      @if($type !== "Pending")
-      <p>Request preccessed at : {{$borrow->request_processed_at}}</p>
-      @endif  
-      @if($type !== ("Pending") && $type !== "Rejected")
-      <p>Deadline : {{$borrow->deadline}}</p>
-      @endif
-      @if($type == ("Returned"))
-      <p>Returned at : {{$borrow->returned_at}}</p>
-      @endif
-    </a>
-    @endforeach
+<div x-data="{ open: false }">
+  <div class="border rounded shadow-lg p-4 mb-5 {{$bgColor}}">
+    <button @click="open = !open" class="flex justify-between w-full items-center px-4 py-2 focus:outline-none">
+      <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" :class="{ 'transform rotate-180': open, 'transform rotate-0': !open }">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+      </svg>
+      <div class="text-2xl mb-3 text-center">{{$type}}</div>
+    </button>
+    <div class="space-y-2"  x-show="open" >
+        @foreach ($borrows as $borrow)
+        <a href="{{route('user.borrows.show', ['borrow'=> $borrow->id])}}" class="flex flex-col border shadow-md mb-2 bg-white p-2">
+            <span>Title: {{$borrow->book->title}}</span>
+            <div class="p-4">
+                <p>Authors: {{$borrow->book->authors}}</p>
+                @if($type !== "Pending")
+                <p>Request processed at : {{$borrow->request_processed_at}}</p>
+                @endif  
+                @if($type !== "Pending" && $type !== "Rejected")
+                <p>Deadline : {{$borrow->deadline}}</p>
+                @endif
+                @if($type == "Returned")
+                <p>Returned at : {{$borrow->returned_at}}</p>
+                @endif
+            </div>
+          </a>
+        @endforeach
+    </div>
+  
+    {{-- <div @click>
+      {{$borrows->links('pagination::tailwind', ['paginator' => $borrows])}}
+    </div> --}}
+  </div>
 </div>
+
